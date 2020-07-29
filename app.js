6c55e370f2b5a094956eb3dea3288cc383a1ac26
MANARE <manare@mintpass.kr>
2020-07-30 00:32:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ var app = express();
 var cors = require("cors");
 
 
-// bodyParser는 미들웨어이기 때문에 라우터 보다 항상 위에 있도록 해야함
 app.use(bodyParser.json());
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
 
-// Mysql DB pool 생성
+
 var pool = mysql.createPool({
             // config.js에 있는 정보를 바탕으로 연결
             host: config.mysql.host,
@@ -26,13 +24,11 @@ var pool = mysql.createPool({
             waitForConnections:false
         });
 
-// Main
+
 app.listen(config.port, function() {
     console.log("Server listening on port %d", config.port);
 });
 
-// Router
-// 기본으로 index.js를 찾기 때문에
-// require("./routes/index.js")라고 명시해주지 않았음
+
 var routes = require("./routes")(app, pool);
 
