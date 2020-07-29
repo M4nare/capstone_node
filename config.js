cb0bc5bb37d74f9ac56521cde05dbc7a7f9eb435
Manare <manare@MacBook-Pro.local>
2020-07-30 00:29:04 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+    port: 3000,
+    mysql: {
+        host: "localhost",
+        port: 3306,
+        db: "capston",
+        username: "",
+        password: "",
+    }
+}
+
+module.exports = config;
+
+
